f354ebf92e606618b99a2cb0451b06e58cdf9ad2
C4e10VeK <tank.t3445@gmail.com>
2021-07-23 03:59:09 +1000

    Версия opengl 4.6

--- src/window/Window.cpp
+++ src/window/Window.cpp
@@ -4,8 +4,8 @@
 
 Window::Window(int width, int height, const std::string &title)
 {
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     m_window = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
