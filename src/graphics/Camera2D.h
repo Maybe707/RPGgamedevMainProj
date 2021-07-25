2e47e6847e97180fa03baab65f646f7a77d297e9
C4e10VeK <tank.t3445@gmail.com>
2021-07-25 21:49:19 +1000

    Add scale for Camera

--- src/graphics/Camera2D.h
+++ src/graphics/Camera2D.h
@@ -11,8 +11,9 @@ private:
     glm::vec2 m_position;
     float m_zn;
     float m_zf;
+    float m_scale;
 public:
-    Camera2D(glm::vec2 position, float width = 0.0f, float height = 0.0f, float zn = 0.0f, float zf = 100.0f);
+    Camera2D(glm::vec2 position, float width = 0.0f, float height = 0.0f, float scale = 0.5f, float zn = 0.0f, float zf = 100.0f);
 
     glm::vec2 getPosition() const;
     void setPosition(glm::vec2 position);
