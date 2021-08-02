e21d55746514e5e83d455526e5e70c49ec867677
C4e10VeK <tank.t3445@gmail.com>
2021-08-02 15:36:54 +1000

    start work on procedural wordl generation

--- src/scripts/WorldGenScript.h
+++ src/scripts/WorldGenScript.h
@@ -3,21 +3,37 @@
 
 #include "../scene/Script.h"
 #include "../scene/components/TileMapComponent.h"
+#include "../graphics/Rect.h"
+#include <glm/gtc/noise.hpp>
+
+struct LvlRoom
+{
+    IntRect roomSize;
+    glm::ivec2 roomPos;
+    glm::ivec2 origin;
+
+    LvlRoom() : roomSize(0, 0, 0, 0), roomPos(0, 0), origin(0, 0) { }
+    LvlRoom(IntRect size, glm::ivec2 pos) : roomSize(size), roomPos(pos) { }
+};
 
 class WorldGenScript : public Script
 {
 private:
     TileMap* m_map = nullptr;
 public:
+    WorldGenScript() { }
+
     void onCreate() 
     {
         m_map = &getComponent<TileMap>();
         
-        for (int x = m_map->bounds.getLeft() * CHUNK_SIZE; x < m_map->bounds.getWidth() * CHUNK_SIZE; x++)
+        for (int x = m_map->globalBounds.getLeft(); x < m_map->globalBounds.getWidth(); x++)
         {
-            for (int y = m_map->bounds.getBottom() * CHUNK_SIZE; y < m_map->bounds.getHeight() * CHUNK_SIZE; y++)
+            for (int y = m_map->globalBounds.getBottom(); y < m_map->globalBounds.getHeight(); y++)
             {
-                m_map->setTile({x, y}, *map::TilesData.at(1));
+                u8 id = glm::perlin(glm::vec3(x * 0.25f, y * 0.05f, 0.0f)) < 0.055f;
+
+                m_map->setTile({x, y}, *TilesData.at(id));
             }
         }
     }
